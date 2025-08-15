f41d0a5bb9c949faa38b00eeb5ff0ff4d48df75e
Pond <Peerapat@callvoice.co.th>
2025-08-15 10:49:28 +0700

    fix

--- model/case_history.go
+++ model/case_history.go
@@ -1,20 +1,19 @@
 package model
 
 import (
-	"encoding/json"
 	"time"
 )
 
 type CaseHistory struct {
-	ID        int             `json:"id" db:"id"`
-	OrgID     string          `json:"orgId" db:"orgId"`
-	CaseID    string          `json:"caseId" db:"caseId"`
-	Username  string          `json:"username" db:"username"`
-	Type      string          `json:"type" db:"type"`
-	FullMsg   string          `json:"fullMsg" db:"fullMsg"`
-	JSONData  json.RawMessage `json:"jsonData" db:"jsonData"` // assuming jsonb in DB
-	CreatedAt time.Time       `json:"createdAt" db:"createdAt"`
-	CreatedBy string          `json:"createdBy" db:"createdBy"`
+	ID        int       `json:"id" db:"id"`
+	OrgID     string    `json:"orgId" db:"orgId"`
+	CaseID    string    `json:"caseId" db:"caseId"`
+	Username  string    `json:"username" db:"username"`
+	Type      string    `json:"type" db:"type"`
+	FullMsg   string    `json:"fullMsg" db:"fullMsg"`
+	JSONData  *string   `json:"jsonData" db:"jsonData"` // assuming jsonb in DB
+	CreatedAt time.Time `json:"createdAt" db:"createdAt"`
+	CreatedBy string    `json:"createdBy" db:"createdBy"`
 }
 
 type CaseHistoryInsert struct {
