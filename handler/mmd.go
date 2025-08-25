74274c6fbdb0e65845496a68261d006b9f0cc5c1
Jane <>
2025-08-25 14:24:03 +0700

    unit

--- handler/mmd.go
+++ handler/mmd.go
@@ -1801,9 +1801,9 @@ func InsertMmdUnit(c *gin.Context) {
 	  "locLon", "locAlt", "locBearing", "locSpeed", "locProvider", "locGpsTime", "locSatellites", "locAccuracy",
 	   "locLastUpdateTime", "breakDuration", "healthChk", "healthChkTime", "sttId", "createdAt", "updatedAt",
 	    "createdBy", "updatedBy")
-	VALUES ($1, $2, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15,
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15,
 	 $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29,
-	  $30, $31, $32, $33, $34, $35)
+	  $30, $31, $32, $33, $34)
 	RETURNING id ;
 	`
 
